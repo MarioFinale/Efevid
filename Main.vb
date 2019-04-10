a1904175347091ddbe600788cde02014c57501de
MarioFinale <MarioFinale@hotmail.com>
2019-04-10 19:49:00 -0400

    Revert "Bugfixes"
    This reverts commit f439670f0cea565a620b1cc5bad6172419dd7df6.

--- Main.vb
+++ Main.vb
@@ -1,7 +1,6 @@
 ﻿Imports MWBot.net
 Imports MWBot.net.WikiBot
 Imports Utils.Utils
-Imports LogEngine
 
 Module Main
     Public ReadOnly Header As String = Exepath & "Res" & DirSeparator & "header.hres"
@@ -9,15 +8,8 @@ Module Main
     Public ReadOnly Hfolder As String = Exepath & "hfiles" & DirSeparator
     Public ReadOnly ImgFolder As String = Exepath & "Images" & DirSeparator
     Public ReadOnly Reqfolder As String = Exepath & "req" & DirSeparator
-    Public ReadOnly ConfigFilePath As String = Exepath & "Config.cfg"
-    Public ReadOnly Logpath As String = Exepath & "VidLog.psv"
-    Public ReadOnly UserPath As String = Exepath & "Users.psv"
-    Public ReadOnly SettingsPath As String = Exepath & "Settings.psv"
-    Public ReadOnly EventLogger As New LogEngine.LogEngine(Logpath, UserPath, "Efevid")
-    Public ReadOnly SettingsProvider As New Settings(SettingsPath)
 
     Sub Main()
-        Dim ESWikiBOT As Bot = New Bot(ConfigFilePath, Logpath)
         Do While True
             If Not IO.Directory.Exists(Reqfolder) Then
                 IO.Directory.CreateDirectory(Reqfolder)
@@ -26,16 +18,17 @@ Module Main
             If IO.File.Exists(Reqfolder & "efevid.runme") Then
                 IO.File.Delete(Reqfolder & "efevid.runme")
                 Try
+                    Log_Filepath = Exepath & "VidLog.psv"
                     Try
-
+                        Dim ESWikiBOT As Bot = New Bot(New ConfigFile(ConfigFilePath), Log_Filepath)
                         Dim igen As New VideoGen(ESWikiBOT)
                         igen.CheckEfe()
                     Catch ex As Exception
                     End Try
                     Threading.Thread.Sleep(6000)
-                    If IO.File.Exists(Logpath) Then
-                        IO.File.Delete(Logpath)
-                        IO.File.Create(Logpath).Close()
+                    If IO.File.Exists(Exepath & "VidLog.psv") Then
+                        IO.File.Delete(Exepath & "VidLog.psv")
+                        IO.File.Create(Exepath & "VidLog.psv").Close()
                     End If
                 Catch ex As Exception
                     EventLogger.EX_Log(ex.Message, "Checker")
